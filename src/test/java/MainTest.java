dbdb254d11848713c466c4feb2786fb7081ae0e8
William <wcao00@stuy.edu>
2018-11-03 19:39:21 -0400

    first test case to ensure JUnit 5 works

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+package src.test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author William Cao
+ * @since 1.0
+ */
+public class MainTest{
+	@Test
+	void ensureTestWorks(){
+		assertEquals(1, 0 + 1);
+	}
+}
