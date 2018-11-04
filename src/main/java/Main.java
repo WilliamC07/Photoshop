9044241eb83bd9b2451c54f7f69be4e416fbe3ec
William <wcao00@stuy.edu>
2018-11-03 23:40:19 -0400

    improved documentation and renamed files for clarity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 package src.main.java;
 
-import src.main.java.ViewController.MainView;
+import src.main.java.viewController.ApplicationStart;
 
 /**
- * Starting point of the program. This should only have one method, the main method.
+ * True starting point of the program. This should only have one method, the main method. This class cannot extend 
+ * javafx.application.Application because JavaFX is run with dependencies. Its only purpose is to call 
+ * src.main.java.viewController.ApplicationStart.
  * 
+ * Related link: http://mail.openjdk.java.net/pipermail/openjfx-dev/2018-June/021977.html
+ * 
+ * @see src.main.java.viewController.ApplicationStart
  * @author William Cao
  * @since 1.0
  */
@@ -13,6 +18,6 @@ public class Main{
 	 * MainView cannot be the starting point of the program because it extends Applications.
 	 */
 	public static void main(String[] args){
-		MainView.main(args);
+		ApplicationStart.main(args);
 	}
 }
--- src/main/java/viewController/ApplicationStart.java
+++ src/main/java/viewController/ApplicationStart.java
@@ -1,17 +1,21 @@
-package src.main.java.ViewController;
+package src.main.java.viewController;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
+ * This class is the real start to the program (it is called by another class (src.main.java.Main)). It should be
+ * the starting point of the program. If any data needs to be read, any GUI content needs to be created, or anything 
+ * else to begin the program, this is the class to do it in. 
+ * 
+ * @see src.main.java.Main
  * @author William Cao
  * @since 1.0
  */
-public class MainView extends Application{
+public class ApplicationStart extends Application{
 
 	/**
 	 * Initialization before the screen is shown. This method is called right after {@link main()}. 
-	 * 
 	 */
 	@Override public void init(){}
 
