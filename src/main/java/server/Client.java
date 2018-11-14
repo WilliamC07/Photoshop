75b469b57c7130099ef4c1cc3ad4dcf0aeae66ec
William <wcao00@stuy.edu>
2018-11-13 21:28:32 -0500

    broke file into parts but there is some broken code somewhere :c

--- src/main/java/server/Client.java
+++ src/main/java/server/Client.java
@@ -1,5 +1,6 @@
-package server;
+//package server;
 
+import java.awt.*;
 import java.io.*;
 import java.net.Socket;
 
@@ -13,23 +14,63 @@ public class Client {
             DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
         ){
             // Get the file. Images are represented by File to make it flexible enough to send xml files
-            File file = new File("image.jpg");
-            int fileSize = (int) file.length();  // The image size should be less than the max size of an int
-
-            // Turn the file into a byte array to be sent through a stream
-            FileInputStream fileInputStream = new FileInputStream(file);
-            byte[] imageRepresentation = new byte[fileSize];
-            fileInputStream.read(imageRepresentation, 0, fileSize);
-
-            // First data is the file size
-            outStream.writeInt(fileSize);
-            // Second data is the actual byte array representation of the image
-            outStream.write(imageRepresentation);
-            // Send the data out
-            outStream.flush();
+            File file = new File("1080p.jpg");
+            sendFile(file, outStream);
         }catch(IOException e){
             e.printStackTrace();
             System.exit(1);
         }
+    }
+
+    public static void sendFile(File file, DataOutputStream dataOutputStream) throws IOException{
+        // File size is needed to create a byte[] which will then be used to create a file
+        int fileSize = (int) file.length();
+
+        // Get byte representation of the image to send through a stream
+        byte[] byteRepresentation = new byte[fileSize];
+        FileInputStream fileStream = new FileInputStream(file);
+        fileStream.read(byteRepresentation, 0, fileSize);  // TODO: Make sure we do real all the bytes
+
+        // We are sending the image by chunks of maximum of 100 000 bytes
+        final int MAX_CHUNK_SIZE = 100_000;
+        int totalChunks = (int) Math.ceil(fileSize / (double) MAX_CHUNK_SIZE);
+        int lastChunkSize = fileSize % MAX_CHUNK_SIZE;
+
+        // Information to generate the image and make sure all the information is received
+        dataOutputStream.writeInt(0); // 0 means we are sending an image
+        dataOutputStream.writeInt(fileSize);
+        dataOutputStream.writeInt(totalChunks);
+        dataOutputStream.writeInt(MAX_CHUNK_SIZE);
+        dataOutputStream.writeInt(lastChunkSize);
+
+        /*System.out.println("Client: "+fileSize);
+        System.out.println("Client: "+totalChunks);
+        System.out.println("Client: "+MAX_CHUNK_SIZE);
+        System.out.println("Client: "+lastChunkSize);*/
+        dataOutputStream.flush();
+
+        // We are then sending the actual image
+        for(int i = 0; i < totalChunks; i++){
+            boolean isLastChunk = i == totalChunks - 1;
+
+            // First part of the stream is the chunk number
+            dataOutputStream.writeInt(i);
+            // Second part is the size of the chunk
+            dataOutputStream.writeInt(isLastChunk ? lastChunkSize : MAX_CHUNK_SIZE);
+            // Send the chunks
+            int start = i * MAX_CHUNK_SIZE;
+            if(isLastChunk){
+                start = (i - 1) * MAX_CHUNK_SIZE;
+            }
+            int end = start + (isLastChunk ? lastChunkSize : MAX_CHUNK_SIZE);
+            System.out.println("start: "+start);
+            System.out.println("end: " +end);
+            dataOutputStream.write(byteRepresentation, start, end);
+
+            dataOutputStream.flush();
+        }
+
+        // Close off sending image
+        dataOutputStream.writeInt(0);
     }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,9 +1,11 @@
-package server;
+//package server;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 
+// TODO: Fix up whatever mess this is
 public class Server {
     /**
      * Temporary
@@ -13,20 +15,72 @@ public class Server {
     public static void main(String[] args) {
         int port = 49153; // This should later be changed to 0 the port is guaranteed to be free
 
+        boolean isReadingImage = false;
+        DownloadImage downloadImage = null;
+
+
         try(ServerSocket serverSocket = new ServerSocket(port);
             Socket socket = serverSocket.accept();
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream())
         ){
-            // Get the byte representation
-            byte[] fileRepresentation = new byte[dataInputStream.readInt()];
-            dataInputStream.read(fileRepresentation);
-
-            // Turn the byte representation into a File
-            FileOutputStream fileOutputStream = new FileOutputStream("imageCopy.jpg");
-            fileOutputStream.write(fileRepresentation, 0, fileRepresentation.length);
+            while(true){
+                if(dataInputStream.readInt() == 0){  // Starting or ending the reading of an image
+                    isReadingImage = !isReadingImage;
+                    if(downloadImage == null){
+                        downloadImage = new DownloadImage(dataInputStream.readInt(), dataInputStream.readInt(),
+                                                          dataInputStream.readInt(), dataInputStream.readInt());
+                    }
+                    // Finish reading
+                    if(!isReadingImage){
+                        downloadImage.generateAndDownloadImage(new File("copy.jpg"));
+                        System.exit(0);
+                    }
+                }else if(isReadingImage){
+                    downloadImage.addChunk(dataInputStream);
+                }
+            }
         }catch(IOException e){
             e.printStackTrace();
             System.exit(0);
+        }
+    }
+
+    private static class DownloadImage{
+        private final int fileSize;
+        private final int totalChunks;
+        private final int MAX_CHUNK_SIZE;
+        private final int lastChunkSize;
+        private final byte[] byteRepresentation;
+
+        DownloadImage(int fileSize, int totalChunks, int MAX_CHUNK_SIZE, int lastChunkSize) {
+            this.fileSize = fileSize;
+            this.totalChunks = totalChunks;
+            this.MAX_CHUNK_SIZE = MAX_CHUNK_SIZE;
+            this.lastChunkSize = lastChunkSize;
+
+            /*System.out.println("server: "+fileSize);
+            System.out.println("server: "+totalChunks);
+            System.out.println("server: "+MAX_CHUNK_SIZE);
+            System.out.println("server: "+lastChunkSize);*/
+            byteRepresentation = new byte[fileSize];
+        }
+
+        void addChunk(DataInputStream dataInputStream) throws IOException{
+            int chunkNumber = dataInputStream.readInt();
+            int chunkSize = dataInputStream.readInt();
+
+            int start = 0;
+            if(chunkNumber != 0){
+                start = (chunkNumber - 1) * MAX_CHUNK_SIZE;
+            }
+            int end = start + chunkSize;
+
+            dataInputStream.read(byteRepresentation, start, end); // TODO: Ensure the read is same as expected chunkSize
+        }
+
+        void generateAndDownloadImage(File file) throws IOException{
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(byteRepresentation);
         }
     }
 }
