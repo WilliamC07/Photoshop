24b258f9240625cb1ef8f39cdddc2a0dc3429ac1
William <wcao00@stuy.edu>
2018-11-16 22:13:56 -0500

    documentation and improved variable names

--- src/main/java/server/ChunkType.java
+++ src/main/java/server/ChunkType.java
@@ -1,33 +1,52 @@
-//package server;
+package server;
 
-public enum ChunkType {
+/**
+ * Enum to identify what type of data is being sent.
+ */
+enum ChunkType {
     /**
      * Value of 0
+     * First chunk sent by a socket connection
      */
     START,
 
     /**
      * Value of 1
+     * Intermediate chunk sent by a socket connection
      */
     DATA,
 
     /**
      * Value of 2
+     * Last chunk sent by a socket connection
      */
     END;
 
+    /**
+     * Give values to the enum values (index position)
+     */
     private final static ChunkType[] ENUMS = ChunkType.values();
 
-    public static ChunkType get(int val){
+    /**
+     * Get the ChunkType object given a magic number
+     * @param val The magic number (index number of {@link #ENUMS}) that represent each enum object
+     * @return The ChunkType Object
+     */
+    static ChunkType get(int val){
         if(val >= ENUMS.length || val < 0){
             throw new IllegalArgumentException("ChunkType number does not exist");
         }
         return ENUMS[val];
     }
 
-    public static int get(ChunkType fileType){
+    /**
+     * Get the magic number of a ChunkType object
+     * @param chunkType ChunkType object to be converted into a magic number
+     * @return Magic number of the given ChunkType Object
+     */
+    static int get(ChunkType chunkType){
         for(int i = 0; i < ENUMS.length; i++){
-            if(ENUMS[i] == fileType){
+            if(ENUMS[i] == chunkType){
                 return i;
             }
         }
