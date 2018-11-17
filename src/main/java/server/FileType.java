7b40516dd4f591521c20e117b17f79426ded5f1a
William <wcao00@stuy.edu>
2018-11-16 22:16:53 -0500

    documentation and made to be part of a package

--- src/main/java/server/FileType.java
+++ src/main/java/server/FileType.java
@@ -1,4 +1,4 @@
-//package server;
+package server;
 
 public enum FileType {
     /**
@@ -14,8 +14,16 @@ public enum FileType {
      */
     MISC;
 
+    /**
+     * Give values to the enum objects (index position)
+     */
     private final static FileType[] ENUMS = FileType.values();
 
+    /**
+     * Get the FileType object given a magic number
+     * @param val The magic number (index number of {@link #ENUMS}) that represent each enum object
+     * @return The FileType Object
+     */
     public static FileType get(int val){
         if(val > ENUMS.length || val < 0){
             throw new IllegalArgumentException("FileType number does not exist");
@@ -23,6 +31,11 @@ public enum FileType {
         return ENUMS[val];
     }
 
+    /**
+     * Get the magic number of a FileType object
+     * @param fileType FileType object to be converted into a magic number
+     * @return Magic number of the given FileType Object
+     */
     public static int get(FileType fileType){
         for(int i = 0; i < ENUMS.length; i++){
             if(ENUMS[i] == fileType){
