202203b24f97e62435734f5039c348adbfe6314e
William <wcao00@stuy.edu>
2018-11-18 14:40:02 -0500

    use new ActionHandler interface and can send original image now

--- src/main/java/network/Server.java
+++ src/main/java/network/Server.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 /**
  * Only one user should have this instance running when sharing a file with collaborators.
  */
-public class Server implements ActionHandler{
+public class Server{
     private ArrayList<Connector> connectors = new ArrayList<>();
 
     /**
@@ -18,7 +18,7 @@ public class Server implements ActionHandler{
         int targetPort = 49153; // TODO: replace with 0, leave it for testing purposes
         // TODO: Make into another thread to allow multiple connections through the same port
         try(ServerSocket serverSocket = new ServerSocket(targetPort)){
-            Connector connector = new Connector(serverSocket.accept(), this);
+            Connector connector = new Connector(serverSocket.accept(), this::handle);
             connector.start();
             connectors.add(connector);
         }catch(IOException e){
@@ -28,15 +28,28 @@ public class Server implements ActionHandler{
     }
 
     /**
-     * Handles the file sent through given the file and action type
+     * Handles the file sent through given the file, file type, action type, and connector
      * Must be synchronized since multiple threads are accessing this
-     *
-     * @see network.ActionType
-     * @param file File to be received
-     * @param actionType What action to be performed
+     * @param file File received
+     * @param fileType File type received
+     * @param actionType Action received
+     * @param connector Connector that delivered the information
+     * @throws IllegalArgumentException Wrong instruction sent
      */
-    @Override public synchronized void handle(File file, ActionType actionType) {
+    private synchronized void handle(File file, FileType fileType, ActionType actionType, Connector connector)
+            throws IllegalArgumentException{
+        switch (actionType) {
+            case SEND_ORIGINAL_IMAGE:
+                // TODO: remove. Testing purposes.
+                File filed = new File("network/4k.png");
+                System.out.println(filed.length());
 
+                connector.sendFile(new Sender(filed, FileType.IMAGE, ActionType.PUSH_INSTRUCTION));
+                System.out.println("sent file from server");
+                break;
+            default:
+                throw new IllegalArgumentException("This is not a server instruction: " + actionType.name());
+        }
     }
 
     /**
