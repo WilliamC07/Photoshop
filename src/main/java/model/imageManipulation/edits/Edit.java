31d64f07531a4f5ed0ca799a4dd476e7ec7bb03a
William <wcao00@stuy.edu>
2018-11-22 09:55:11 -0500

    made into an abstract class instead of interface

--- src/main/java/model/imageManipulation/edits/Edit.java
+++ src/main/java/model/imageManipulation/edits/Edit.java
@@ -3,11 +3,11 @@ package model.imageManipulation.edits;
 import javafx.scene.image.PixelWriter;
 
 /**
- * All instructions should implement this method.
+ * All instructions should extend this class.
  * 
  * Images can be manipulated using WritableImage through PixelWriter. ImageBuilder holds the WritableImage and is 
- * edited indirectly using WritableImage's PixelWriter. ImageBuilder looks for this interface. See more instructions
- * on that class.
+ * edited indirectly using WritableImage's PixelWriter. ImageBuilder looks for this abstract method. See more
+ * instructions on that class.
  *
  * @see model.imageManipulation.edits.ImageBuilder
  * @see javafx.scene.image.WritableImage
@@ -15,10 +15,10 @@ import javafx.scene.image.PixelWriter;
  * @author William Cao
  * @since 1.0
  */
-interface Edit{
+public abstract class Edit{
 	/**
 	 * This method will be the one directly working with PixelWriter from ImageBuilder. It should also be the only
 	 * package-private method (other than the constructor). Everything else should be private (fields, methods, ect.) 
 	 */
-	void change(PixelWriter pixelWriter);
+	abstract void change(PixelWriter pixelWriter);
 }
