965112265262bb45148a0f9c175e67af4238803e
William <wcao00@stuy.edu>
2018-11-22 22:50:05 -0500

    (Last commit was a mistake) application starts with welcome screen

--- src/main/java/views/WelcomeScreen.java
+++ src/main/java/views/WelcomeScreen.java
@@ -1,14 +1,17 @@
 package views;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.DirectoryChooser;
@@ -21,76 +24,157 @@ import java.io.File;
 import java.io.IOException;
 
 class WelcomeScreen extends VBox {
+    private final int TITLE_FONT_SIZE = 30;
+    private final int HEADER_FONT_SIZE = 20;
+    private final int TEXT_SIZE = 13;
+
     WelcomeScreen(){
         this.setAlignment(Pos.CENTER);
         this.setPrefSize(ScreenDimensions.welcomeWidth, ScreenDimensions.welcomeHeight);
-        generateView();
+
+        makeTitle();
+        makeCreatorOption();
+        makeOpenerOption();
     }
 
-    private void generateView(){
-        // Title
+    /**
+     * Makes the title label and a separator at the top of the view.
+     */
+    private void makeTitle(){
         Label welcomeLabel = new Label("Photoshop Java");
-        welcomeLabel.setFont(new Font(30));
+        welcomeLabel.setFont(new Font(TITLE_FONT_SIZE));
+
         Separator separator = new Separator(Orientation.HORIZONTAL);
-        Utility.resizeWidth(separator, ScreenDimensions.welcomeWidth);
+        separator.setMaxWidth(Double.MAX_VALUE);  // Takes up the entire width
 
-        int containersHeight = ScreenDimensions.welcomeHeight / 2 - 50;
+        getChildren().addAll(welcomeLabel, separator);
+    }
 
-        // Create a new project
-        Label newProjectHeader = new Label("Create a new project");
-        newProjectHeader.setFont(new Font(20));
-        Label newProjectDirection = new Label("Project name:");
-        TextField newProjectName = new TextField();
-        Button newProjectButton = new Button(">");
-        HBox newProjectChooserContainer = new WHBox(newProjectDirection, newProjectName, newProjectButton);
-        newProjectChooserContainer.setAlignment(Pos.CENTER);
-        Utility.resizeHeight(newProjectChooserContainer, containersHeight);
-        Label newProjectError = new Label();
+    /**
+     * Makes the section that allows the user to make a project and a separator from the other container.
+     * This goes below {@link #makeTitle()}.
+     */
+    private void makeCreatorOption(){
+        // Section that allows the user to make a project
+        Label header = new Label("Create a new project");
+        Label inputLabel = new Label("Project name:");
+        TextField inputField = new TextField();
+        Button makeProjectButton = new Button(">");
+        Label errorLabel = new Label();
+        HBox makerContainer = new HBox(inputLabel, inputField, makeProjectButton);
 
-        // Or separator
-        int separatorWidth = ScreenDimensions.welcomeWidth / 2 - 25;
+        // header design
+        header.setFont(new Font(HEADER_FONT_SIZE));
+
+        // inputLabel design
+        inputLabel.setFont(new Font(TEXT_SIZE));
+
+        // Input field design and action
+        inputField.setFont(new Font(TEXT_SIZE));
+        inputField.setOnKeyPressed(k -> {
+            if (k.getCode() == KeyCode.ENTER) {
+                createProject(inputField.getText(), errorLabel);
+            }
+        });
+
+        // makeProjectButton design and action
+        makeProjectButton.setOnAction(e -> createProject(inputField.getText(), errorLabel));
+
+        // container design
+        makerContainer.setPadding(new Insets(5));  // The nodes won't touch each other and appear clumped together
+        makerContainer.setAlignment(Pos.CENTER);
+        VBox.setVgrow(makerContainer, Priority.ALWAYS); // Divide the WelcomeScreen screen space evenly with other containers
+
+        // Divider to separate the project maker from the other section
         Separator s1 = new Separator();
-        Utility.resizeWidth(s1, separatorWidth);
         Label orLabel = new Label(" or "); // Spaces so separators don't touch the word
-        orLabel.setFont(new Font(20));
         Separator s2 = new Separator();
-        Utility.resizeWidth(s2, separatorWidth);
         HBox orSeparatorContainer = new HBox(s1, orLabel, s2);
-        Utility.resizeWidth(orSeparatorContainer, ScreenDimensions.welcomeWidth);
+
+        // Make the separators take up the greatest amount of space while being the same size
+        HBox.setHgrow(s1, Priority.ALWAYS);
+        HBox.setHgrow(s2, Priority.ALWAYS);
+
+        // Make the separators in the middle of orLabel
         orSeparatorContainer.setAlignment(Pos.CENTER);
 
-        // Open existing project
-        Label oldProjectHeader = new Label("Open existing project");
-        oldProjectHeader.setFont(new Font(20));
-        Button oldProjectChooser = new Button("Choose file");
-        Label chosenFile = new Label();
-        oldProjectChooser.setOnAction(e -> {
-            DirectoryChooser directoryChooser = new DirectoryChooser();
-            directoryChooser.setTitle("Open existing project");
-            File file = directoryChooser.showDialog(this.getScene().getWindow());
-            chosenFile.setText(file.getAbsolutePath());
-        });
-        Button openOldProject = new Button(">");
+        // Or text design
+        orLabel.setFont(new Font(20));
 
-        HBox oldProjectContainer = new HBox(oldProjectChooser, chosenFile, openOldProject);
-        oldProjectContainer.setAlignment(Pos.CENTER);
-        Utility.resizeHeight(oldProjectContainer, containersHeight);
+        getChildren().addAll(makerContainer, errorLabel, orSeparatorContainer);
+    }
+
+    /**
+     * Makes the section that allows the user to either open an already existing project or connect to a server.
+     */
+    private void makeOpenerOption(){
+        Label header = new Label("Open an existing project");
+
+        Button openDirectoryChooserButton = new Button("Choose Project");
+        // Need a wrapper to allow centering of the button
+        VBox openDirectoryContainer = new VBox(openDirectoryChooserButton);
+
+        Separator separator = new Separator(Orientation.VERTICAL);
+
+        TextField ipInputField = new TextField();
+        TextField portInputField = new TextField();
+        Button connectButton = new Button("Connect");
+        VBox connectToServerContainer = new VBox(ipInputField, portInputField, connectButton);
+
+        HBox headContainer = new HBox(openDirectoryContainer, separator, connectToServerContainer);
+
+        // header design
+        header.setFont(new Font(HEADER_FONT_SIZE));
+
+        // Centering openDirectoryChooserButton
+        openDirectoryContainer.setAlignment(Pos.CENTER);
+
+        // Centering items for connectToServerContainer
+        connectToServerContainer.setAlignment(Pos.CENTER);
+
+        // Connecting to server text fonts and text
+        ipInputField.setFont(new Font(TEXT_SIZE));
+        ipInputField.setPromptText("IP address");
+        ipInputField.setMaxWidth(ScreenDimensions.welcomeWidth / 4.0);
+        portInputField.setFont(new Font(TEXT_SIZE));
+        portInputField.setPromptText("Port");
+        portInputField.setMaxWidth(ScreenDimensions.welcomeWidth / 8.0);
+
+        // Head container spacing nodes
+        // Have to set pref width to actually make it center (connectToServerContainer would take up too much space)
+        headContainer.setAlignment(Pos.CENTER);
+        HBox.setHgrow(connectToServerContainer, Priority.ALWAYS);
+        connectToServerContainer.setPrefWidth(ScreenDimensions.welcomeWidth / 2.0);
+        HBox.setHgrow(openDirectoryContainer, Priority.ALWAYS);
+        openDirectoryContainer.setPrefWidth(ScreenDimensions.welcomeWidth / 2.0);
 
 
-        newProjectButton.setOnAction(e -> createProject(newProjectName.getText(), newProjectError));
+        // Make the container take up space equally
+        VBox.setVgrow(headContainer, Priority.ALWAYS);
 
-        this.getChildren().addAll(welcomeLabel, separator,
-                                  newProjectHeader, newProjectChooserContainer, newProjectError,
-                                  orSeparatorContainer,
-                                  oldProjectHeader, oldProjectContainer);
+        getChildren().addAll(header, headContainer);
     }
 
     private void createProject(String projectName, Label errorLabel){
+        if(projectName.isBlank()){
+            errorLabel.setText("Enter a project name");
+            return;
+        }
+
         try{
             ProjectFactory.createProject(projectName);
         }catch(IOException e){
-            e.printStackTrace();
             errorLabel.setText("Please enter a unique valid project name");
         }
+    }
+
+    /**
+     * Opens the DirectoryChooser to allow the user to open a project. If the directory is a valid project, it will
+     * open that project. If it is not, it will inform the user through the label given
+     * @param errorLabel Label to output the error message to the user
+     */
+    private void showDirectoryChooser(Label errorLabel){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File file = directoryChooser.showDialog(this.getScene().getWindow());
     }
 }
