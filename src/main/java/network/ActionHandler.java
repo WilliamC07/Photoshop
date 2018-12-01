0de1cd53731f1f48c728439d2b78b3fec824c0cf
William <wcao00@stuy.edu>
2018-12-01 15:09:29 -0500

    simplified handling information received

--- src/main/java/network/ActionHandler.java
+++ src/main/java/network/ActionHandler.java
@@ -8,17 +8,38 @@ import java.io.File;
  * @see network.Connector
  * @see network.ActionType
  */
-@FunctionalInterface
 interface ActionHandler {
     /**
-     * Handles the file sent through given the file, file type, action type, and connector
-     * 
-     * @param file file received
-     * @param message Can be null if and only if file is null.
-     * @param fileType file type received
-     * @param actionType action type received.
-     * @param connector Connector that delivered the information
-     * @throws IllegalArgumentException Wrong instruction sent
+     * If only a message, ActionType are received will this be called.
+     * @param message String value of data received
+     * @param actionType Action to be done by the receiving end
+     * @param connector Reference to the socket connection
      */
-    void handle(File file, String message, FileType fileType, ActionType actionType, Connector connector) throws IllegalArgumentException;
+    void handle(String message, ActionType actionType, Connector connector);
+
+    /**
+     * If only a file, ActionType are received will this be called.
+     * @param file File received
+     * @param fileType FileType received.
+     * @param actionType Action to be done by the receiving end
+     * @param connector Reference to the socket connection
+     */
+    void handle(byte[] file, FileType fileType, ActionType actionType, Connector connector);
+
+    /**
+     * If only a action is received
+     * @param actionType Action to be done by the receiving end
+     * @param connector Reference to the socket connection
+     */
+    void handle(ActionType actionType, Connector connector);
+
+    /**
+     * If a file, message, and ActionType is received.
+     * @param file File received
+     * @param fileType FileType received.
+     * @param message String value of data received
+     * @param actionType Action to be done by the receiving end
+     * @param connector Reference to the socket connection
+     */
+    void handle(byte[] file, FileType fileType, String message, ActionType actionType, Connector connector);
 }
