a74b273c86348b2f188f6dffd57e635f044275c0
William <wcao00@stuy.edu>
2018-12-25 11:38:53 -0500

    fixed bug of not converting javafx color to hex correctly

--- src/main/java/model/imageManipulation/edits/ColorUtility.java
+++ src/main/java/model/imageManipulation/edits/ColorUtility.java
@@ -4,6 +4,8 @@ import javafx.scene.paint.Color;
 
 class ColorUtility {
     static String toHex(Color color){
-        return String.format("#%02X%02X%02X", (int) color.getRed(), (int) color.getGreen(), (int) color.getBlue());
+        // must multiply by 255 because getCOLOR() is from 0 to 1 inclusive (percent of 255)
+        return String.format("#%02X%02X%02X", (int) (color.getRed() * 255),
+                (int) (color.getGreen() * 255), (int) (color.getBlue() * 255));
     }
 }
