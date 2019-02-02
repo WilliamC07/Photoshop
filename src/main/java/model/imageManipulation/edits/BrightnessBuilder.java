fa60741a63c012c0a5ba3b3decd4d4c094659067
William Cao <wcao00@stuy.edu>
2019-02-02 16:42:40 -0500

    made brightness builder

---
+++ src/main/java/model/imageManipulation/edits/BrightnessBuilder.java
@@ -0,0 +1,15 @@
+package model.imageManipulation.edits;
+
+public class BrightnessBuilder extends AbstractEditBuilder{
+
+
+    @Override
+    Edit convertDiskInfoToEdit(String[] parts) {
+        return null;
+    }
+
+    @Override
+    public void makeEdit() {
+
+    }
+}
